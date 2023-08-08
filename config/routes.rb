bcea6026627b0132f91ff5331c9661cc59562c26
JoseHiro <ipho.7johero@icloud.com>
2023-08-08 22:29:14 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'home#Index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
