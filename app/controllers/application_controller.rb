6974111389609b61f0af788ce6e7000410a15daa
JoseHiro <ipho.7johero@icloud.com>
2023-08-09 22:57:00 +0900

    Devise simple login function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters               ## accepted key
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:username, :phone_number, :full_name])
+  end
+
+  ## strong parameter gets paramteres from web considiering if the data is safe
+  ## avoids un necessary data (un safe data)
 end
