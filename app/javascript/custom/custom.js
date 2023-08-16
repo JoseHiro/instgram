fffcc68fc883420a4d53834f0324996ad07a6549
JoseHiro <ipho.7johero@icloud.com>
2023-08-16 16:53:25 +0900

    model like edit

--- app/javascript/custom/custom.js
+++ app/javascript/custom/custom.js
@@ -1,15 +1,23 @@
-import * as FilePond from 'filepond';
+import * as FilePond from "filepond";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
 
-import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+document.addEventListener("turbo:load", loadFilePond);
 
-FilePond.registerPlugin(FilePondPluginImagePreview);
+function loadFilePond() {
+  FilePond.registerPlugin(
+    FilePondPluginImagePreview,
+    FilePondPluginFileValidateType
+  );
 
-const inputElement = document.querySelector('#post-images');
+  const inputElement = document.querySelector("#post-images");
 
-// Create a FilePond instance
-const pond = FilePond.create(inputElement, {
-  credits: {},
-  storeAsFile: true,
-  allowMultiple: true,
-  allowReorder: true
-});
+  // Create a FilePond instance
+  const pond = FilePond.create(inputElement, {
+    credits: {},
+    storeAsFile: true,
+    allowMultiple: true,
+    allowReorder: true,
+    acceptedFileTypes: ["image/*"],
+  });
+}
